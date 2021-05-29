1ff7290fb0e9f7dc0ba8423fc606690da27ec197
unknown <gabriel.macedo99@hotmail.com>
2021-05-29 15:54:36 -0300

    Metodos de lista

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carregando apenasd um usuario
+// $root = new Usuario();
+// $root->loadById(3);
 
-$root->loadById(3);
+// echo $root;
 
-echo $root;
+// Carrega uma lista de usuario
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
+// Carrega uma lista de usuarios pelo login
+// $search = Usuario::search('J');
+// echo json_encode($search);
+
+// Carrega um usuario pelo seu login e senha
+
+$usuario = new Usuario();
+$usuario->login("user", "12345");
+echo ($usuario);
 ?>
